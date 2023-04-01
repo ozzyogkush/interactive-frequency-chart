dde339ff2eb242f03208b8471860e39ad85872d3
Derek Rosenzweig <derek.rosenzweig@gmail.com>
2023-04-01 05:01:10 -0400

    Clean up the extraction script a bit, add README

---
+++ README.md
@@ -0,0 +1,4 @@
+This project is initially an archive of https://alexiy.nl/eq_chart/main_display.htm which was in turn a backup of
+the original from "Independent Recording Network", which sadly is no longer available on the web.
+
+This project will likely grow into something more useful down the line.
--- bin/get_sources.mjs
+++ bin/get_sources.mjs
@@ -1,6 +1,59 @@
 import { mkdir, readFile, writeFile } from 'fs/promises';
 import path from 'path';
 
+const processEntry = async entry => {
+  const success = [];
+  const errors = [];
+
+  const { request, response } = entry;
+  const { url: originalUrl } = request;
+  const { content, redirectURL, status } = response;
+
+  const url = new URL(originalUrl);
+  const finalPathName = url.pathname.replace(/^\//, 'extracted/');
+  const file = path.resolve(path.dirname('../'), finalPathName);
+
+  console.log(`Processing file: '${file}'...`);
+  if (![200, 301, 308].includes(status)) {
+    errors.push({ file, originalUrl, redirectURL, error: `Original status is ${status}` });
+    return { success, errors };
+  }
+
+  const dir = file.split('/');
+  dir.pop();
+  console.debug('\tcreating dirs if not present...');
+  try {
+    await mkdir(dir.join('/'), { recursive: true });
+  } catch (e) {
+    errors.push({ file, originalUrl, error: `failed to create directories: ${e.message}` })
+    return { success, errors };
+  }
+
+  let data = content.text;
+  if (data === undefined || content.encoding === 'base64') {
+    try {
+      let urlToFetch = originalUrl;
+      if (300 <= status && status < 400 && redirectURL.length > 0) urlToFetch = redirectURL;
+      console.log(`\tfetching backup file from '${urlToFetch}'...`);
+
+      const { body } = await fetch(urlToFetch);
+      data = body;
+    } catch (e) {
+      errors.push({ file, originalUrl, error: `failed to fetch from original URL: ${e.message}` });
+    }
+  }
+
+  try {
+    console.debug('\twriting file...');
+    await writeFile(file, data);
+    success.push({ file, originalUrl, message: 'file successfully written' })
+  } catch (e) {
+    errors.push({ file, originalUrl, error: `failed to create file: ${e.message}`, data })
+  }
+
+  return { success, errors };
+}
+
 const dewIt = async (harFile, logLevel = 'info') => {
   const summary = {
     success: [],
@@ -12,42 +65,9 @@ const dewIt = async (harFile, logLevel = 'info') => {
   const { entries } = data.log;
 
   for (const entry of entries) {
-    const { request, response } = entry;
-    const { url: originalUrl } = request;
-    const { content } = response;
-
-    const url = new URL(originalUrl);
-    const finalPathName = url.pathname.replace(/^\//, 'extracted/');
-    const file = path.resolve(path.dirname('../'), finalPathName);
-
-    console.log(`Processing file: '${file}'...`);
-    const dir = file.split('/');
-    dir.pop();
-    console.debug('\tcreating dirs if not present...');
-    try {
-      await mkdir(dir.join('/'), { recursive: true });
-    } catch (e) {
-      summary.errors.push({ file, originalUrl, error: `failed to create directories: ${e.message}` })
-    }
-
-    let data = content.text;
-    if (data === undefined || content.encoding === 'base64') {
-      try {
-        console.log(`\tfetching backup file from '${originalUrl}'...`);
-        const { body } = await fetch(originalUrl);
-        data = body;
-      } catch (e) {
-        summary.errors.push({ file, originalUrl, error: `failed to fetch from original URL: ${e.message}` });
-      }
-    }
-
-    try {
-      console.debug('\twriting file...');
-      await writeFile(file, data);
-      summary.success.push({ file, originalUrl, message: 'file successfully written' })
-    } catch (e) {
-      summary.errors.push({ file, originalUrl, error: `failed to create file: ${e.message}`, data })
-    }
+    const { success, errors } = await processEntry(entry);
+    summary.success.push(...success);
+    summary.errors.push(...errors);
   }
   return summary;
 }
